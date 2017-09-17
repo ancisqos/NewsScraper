e4e3b4ec40b147080618304e3a80463a54762cd0
Francis <ancis.arcede@gmail.com>
2017-09-17 19:45:10 -0400

    mongo logic in controllers

---
+++ controllers/headlines.js
@@ -0,0 +1,45 @@
+// controller for headlines
+
+// require scrape and makeDate scripts
+const scrape = require('../scripts/scrape');
+const makeDate = require('../scripts/date');
+
+// require headline and note mongoose models
+const Headline = require('../models/Headline');
+
+module.exports = {
+	fetch: function(cb) {
+
+		// run scrape function
+		scrape(function(data){
+			// data is array of article objects with headlines and summaries
+			const articles = data;
+			// makes sure each article object has date and not saved by default
+			for (var i=0; i<articles.length; i++) {
+				articles[i].date = makeDate();
+				articles[i].saved = false;
+			}
+			Headline.collection.insertMany(articles, { ordered: false }, function(err, docs) {
+				cb(err, docs);
+			});
+		});
+	},
+	delete: function(query, cb){
+		Headline.remove(query, cb);
+	},
+	get: function(query, cb) {
+		// prepare query to get scraped data
+		Headline.find(query)
+		.sort({
+			_id: -1
+		})
+		.exec(function(err, doc){
+			cb(doc);
+		});
+	},
+	update: function(query, cb){
+		Headline.update({_id: query._id}, {
+			$set: query
+		}, {}, cb);
+	}
+};
