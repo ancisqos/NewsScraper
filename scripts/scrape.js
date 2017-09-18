32a3d7a868419e0bd4fd500530a484a1c4541cd3
Francis <ancis.arcede@gmail.com>
2017-09-18 10:29:23 -0400

    trying to scrape contents of body of html

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -7,17 +7,17 @@ var cheerio = require('cheerio');
 // function scrapes zergnet website. cb is callback.
 var scrape = function(cb) {
 	// use request package to take body of page's html
-	request ("http://zergnet.com/", function(err, res, html) {
+	request ("http://zergnet.com/", function(err, res, body) {
 		// body is actual HTML on page. load into cheerio
 
 		// saving to $ creates virtual HTML page
-		var $ = cheerio.load(html);
+		var $ = cheerio.load(body);
 
 		// make empty array to save article info
 		var articles = [];
 
 		// find and loop through each element with theme-summary class
-		$('.text').each(function(i, element){
+		$('.title-box').each(function(i, element){
 
 			var head = $(this).children('.post-title').text().trim();
 
@@ -26,7 +26,7 @@ var scrape = function(cb) {
 
 		      // grab any children with span and grab its inner text
 		      // store this to the sum variable. This is the article summary
-		      var sum = $(this).children("span").text().trim();
+		      var sum = $(this).children(".text").text().trim();
 
 		      // So long as our headline and sum and url aren't empty or undefined, do the following
 		      if (head && sum && url) {
