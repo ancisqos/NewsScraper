b6a56fdbe92e0cdcbc5eba0f6d18506c59ca2924
Francis <ancis.arcede@gmail.com>
2017-09-18 00:47:29 -0400

    trying to figure out which elements to scrape

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -7,26 +7,26 @@ var cheerio = require('cheerio');
 // function scrapes reddit website. cb is callback.
 var scrape = function(cb) {
 	// use request package to take body of page's html
-	request ("https://www.allaboutthejersey.com", function(err, res, body) {
+	request ("https://www.allaboutthejersey.com/", function(err, res, html) {
 		// body is actual HTML on page. load into cheerio
 
 		// saving to $ creates virtual HTML page
-		var $ = cheerio.load(body);
+		var $ = cheerio.load(html);
 
 		// make empty array to save article info
 		var articles = [];
 
 		// find and loop through each element with theme-summary class
-		$('.theme-summary').each(function(i, element){
+		$('.c-entry-box--compact__body').each(function(i, element){
 
-			var head = $(this).children('.story-heading').text().trim();
+			var head = $(this).children('.c-entry-box--compact__title').text().trim();
 
 			 // Grab the URL of the article
-      		var url = $(this).children(".story-heading").children("a").attr("href");
+      		var url = $(this).children(".c-entry-box--compact__title").children("a").attr("href");
 
 		      // Then we grab any children with the class of summary and then grab it's inner text
 		      // We store this to the sum variable. This is the article summary
-		      var sum = $(this).children(".summary").text().trim();
+		      var sum = $(this).children(".p-dek c-entry-box--compact__dek").text().trim();
 
 		      // So long as our headline and sum and url aren't empty or undefined, do the following
 		      if (head && sum && url) {
