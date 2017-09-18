8952737a4b9366d29d4df4d7d423304df09ff4a3
Francis <ancis.arcede@gmail.com>
2017-09-17 21:41:18 -0400

    reviewed date script

---
+++ scripts/date.js
@@ -0,0 +1,20 @@
+// makeDate script
+
+// function that makes formatted date for scraped data
+const makeDate = function(){
+	//save current date to d
+	const d = new Date();
+	// prepare empty string for formatted state
+	const formattedDate = "";
+	// take string and concatenate current month of d
+	formattedDate += (d.getMonth() + 1) + "_";
+	// get number of day in month from d and concatenate it to formatted date string
+	formattedDate += d.getDate() + "_";
+	// get full year and add to formatted date string
+	formattedDate += d.getFullYear();
+	// return formatted date
+	return formattedDate:
+};
+
+// export makeDate function so other backend files can use
+module.exports = makeDate;
